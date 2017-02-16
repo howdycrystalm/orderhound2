f123d30b203a26ac97bf31845f24773c2616f1c3
howdycrystalm <howdycrystalm@gmail.com>
2017-02-16 15:36:58 -0800

    starting fresh

--- db/track_po/update_po_location.sql
+++ db/track_po/update_po_location.sql
@@ -1,9 +1,5 @@
 /*po has been entered in the db from a previous user/employee.
-  this updates current location of po?
-
-same as checkin_sql
-*/
-
+this updates current location of po*/
 UPDATE in_production
 SET date = $1
 WHERE po_num = $2
