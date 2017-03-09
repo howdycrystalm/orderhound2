3f8214f26fb1514f1e7c2f70de6bb11a4f35d6f9
howdycrystalm <howdycrystalm@gmail.com>
2017-03-09 01:25:16 -0800

    add back correct folders

--- public/js/app.js
+++ public/js/app.js
@@ -1,93 +1,74 @@
-  angular.module('houndApp', ['ui.router'])
-      .config(function($stateProvider, $urlRouterProvider) {
-  // INITILIZE STATES
-  // ============================================================
-  $urlRouterProvider.otherwise('/');
-  $stateProvider
-    // LOGIN STATE
-    .state('login', {
-      url: '/',
-      templateUrl: './routes/login.html',
-      controller: 'loginCtrl'
-    })
-    //HOME STATE
-    .state('home', {
-      url: '/home',
-      templateUrl: './routes/home.html',
-      controller: 'homeCtrl',
-      // resolve: {
-      //   user: function(mainService, $state) {
-      //     return mainService.getCurrentUser()
-      //       .then(function(response) {
-      //         if (!response.data.email) {
-      //           return $state.go('/');
-      //         }
-      //         return response.data
-      //       }).catch(function(err) {
-      //         $state.go('/');
-      //       });
-      //   }
-      // }
-    })
-    //ADMIN STATE
-    .state('adminHome', {
-      url: '/adminHome',
-      templateUrl: './routes/adminHome.html',
-      controller: 'adminHomeCtrl',
-      resolve: {
-        user: function(mainService, $state) {
-          return mainService.getCurrentUser()
-            .then(function(response) {
-              if (!response.data.email) {
-                return $state.go('/');
-              }
-              return response.data
-            }).catch(function(err) {
-              $state.go('/');
-            });
-        }
+angular.module('houndApp', ['ui.router'])
+    .config(function($stateProvider, $urlRouterProvider) {
+// INITILIZE STATES
+// ============================================================
+$urlRouterProvider.otherwise('/');
+$stateProvider
+  // LOGIN STATE
+  .state('login', {
+    url: '/',
+    templateUrl: './routes/login.html',
+    controller: 'loginCtrl'
+  })
+  //HOME STATE
+  .state('home', {
+    url: '/home',
+    templateUrl: './routes/home.html',
+    controller: 'homeCtrl',
+    resolve: {
+      user: function(mainService, $state) {
+        return mainService.getCurrentUser()
+          .then(function(response) {
+            if (!response.data.email) {
+              return $state.go('/');
+            }
+            return response.data
+          }).catch(function(err) {
+            $state.go('/');
+          });
       }
-    })
-    //MANAGE
-    .state('manageUser', {
-      url: '/manageUser',
-      templateUrl: './routes/manageUser.html',
-      controller: 'manageUserCtrl',
-      resolve: {
-        user: function(mainService, $state) {
-          return mainService.getCurrentUser()
-            .then(function(response) {
-              if (!response.data.email) {
-                return $state.go('/');
-              }
-              return response.data
-            }).catch(function(err) {
-              $state.go('/');
-            });
-        }
+    }
+  })
+  //ADMIN STATE
+  .state('adminHome', {
+    url: '/adminHome',
+    templateUrl: './routes/adminHome.html',
+    controller: 'adminHomeCtrl',
+    resolve: {
+      user: function(mainService, $state) {
+        return mainService.getCurrentUser()
+          .then(function(response) {
+            if (!response.data.email) {
+              return $state.go('/');
+            }
+            return response.data
+          }).catch(function(err) {
+            $state.go('/');
+          });
       }
-    // DIRECTIVES
-    // ============================================================
+    }
+  })
+  //MANAGE
+  .state('manageUser', {
+    url: '/manageUser',
+    templateUrl: './routes/manageUser.html',
+    controller: 'manageUserCtrl',
+    resolve: {
+      user: function(mainService, $state) {
+        return mainService.getCurrentUser()
+          .then(function(response) {
+            if (!response.data.email) {
+              return $state.go('/');
+            }
+            return response.data
+          }).catch(function(err) {
+            $state.go('/');
+          });
+      }
+    }
+  })
 
-    })
-    .state('header', {
-      url: '/header',
-      templateUrl: '../routes/directives/headerTmpl.html',
-      controller: 'headerCtrl',
-      resolve: {
-        user: function(mainService, $state) {
-          return mainService.getCurrentUser()
-            .then(function(response) {
-              if (!response.data.email) {
-                return $state.go('/');
-              }
-              return response.data
-            }).catch(function(err) {
-              $state.go('/');
-            });
-        }
-      }
-    })
-
+// ASSIGN OTHERWISE
+// ============================================================
 
 });
--- public/js/directives/header/headerCtrl.js
+++ public/js/directives/header/headerCtrl.js
@@ -2,4 +2,3 @@ angular.module('houndApp')
   .controller('headerCtrl', function($scope, $state, mainService, user) {
     $scope.user = user;
 });
-
